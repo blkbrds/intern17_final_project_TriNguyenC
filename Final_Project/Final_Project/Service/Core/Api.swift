aed24a09ed73c22fdbef4e5358c6be76814fe07d
Tri Nguyen C <tri.nguyen@monstar-lab.com>
2022-06-06 20:36:26 +0700

    navigation bar delete all realm

--- Final_Project/Final_Project/Service/Core/Api.swift
+++ Final_Project/Final_Project/Service/Core/Api.swift
@@ -11,7 +11,7 @@ import Alamofire
 final class Api {
 
     struct Path {
-        static let apiKey: String = "0783173ec6d54087b786830fd68a17ed"
+        static let apiKey: String = "4b39660ad8544eb5a9c011157f6b42a6"
         static let baseURL: String = "https://newsapi.org/v2"
         static var searchNews: String { return baseURL + "/everything" }
         static var topHeadlineNews: String { return baseURL + "/top-headlines"}
